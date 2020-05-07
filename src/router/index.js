0b7c78ce2c6f9831230b50590da25ccb14516fdf
monikaa1111 <1342991058@qq.com>
2020-05-07 16:30:22 +0800

    web39-

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import denglu from '@/views/denglu'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+    {
+      path: '/denglu',
+      name: 'denglu',
+      component: denglu
+    },
   ]
 })
---
+++ src/views/BQ/denglu.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="box1">
+        <div>
+            <h2 class="d1">登录</h2>
+        </div>
+        <input type="text" class="d2" placeholder="您的邮箱">
+        <input type="text" class="d2 d3" placeholder="您的密码">
+        <p class="d4">没有账号？快速注册</p>
+        <p class="d5">找回密码</p>
+        <input type="button" class="d5" value="登录">
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.box1{
+    width: 500px;
+    height: 300px;
+    border: 1px solid black;
+    margin: 140px auto;
+    position: relative;
+    top: 0%;
+    left: 0;
+}
+.d1{
+    text-align: center;
+}
+.d2{
+    width: 360px;
+    height: 40px;
+    border-radius: 5px;
+    border:1px solid black;
+    position: absolute;
+    top: 26%;
+    left: 13%;
+    text-indent:1em
+}
+.d3{
+     position: absolute;
+    top: 50%;
+    left: 13%;
+}
+.d4{
+    font-size: 14px;
+    position: absolute;
+    top: 64%;
+    left: 13%;
+}
+.d5{
+    font-size: 14px;
+    position: absolute;
+    top: 64%;
+    left: 72%;
+}
+.d5{
+    width: 300px;
+    height: 40px;
+    position: absolute;
+    top: 78%;
+    left: 19%;
+    }
+</style>
